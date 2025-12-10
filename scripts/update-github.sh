368125231210ff3244b30b22b0421b9cd3f15a72
Ubuntu <ubuntu@ip-172-31-33-155.ec2.internal>
2025-12-10 20:30:31 +0000

    Update: 2025-12-10 20:30:31

--- scripts/update-github.sh
+++ scripts/update-github.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+TOKEN=$(cat ~/environment/_github_token_tonanuvem)
+REPO_URL=$(git remote get-url origin | sed 's|https://|https://tonanuvem:'"$TOKEN"'@|')
+
 git add .
 git commit -m "Update: $(date '+%Y-%m-%d %H:%M:%S')"
-git push origin main
+git push "$REPO_URL" main
